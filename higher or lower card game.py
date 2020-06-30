0638124ffbd6fdb3c3ea46bbc35f18f24360ee2a
Evans96 <rhys.e.evans@outlook.com>
2020-06-30 17:25:42 +0100

    a simple higher or lower game

---
+++ higher or lower card game.py
@@ -0,0 +1,27 @@
+import random as r
+
+x = True
+n = r.randint(0, 13)
+score = 0
+while x == True:
+    c = r.randint(0, 13)
+    while c==n:
+        c=r.randint(0,13)
+    print('The current number is ', n, )
+    ques = input(' higher or lower?')
+    if ques == 'higher':
+        x = bool(c > n)
+        n = c
+        if x == True:
+            score = score + 1
+            print('correct your correct score is', score)
+        if x == False:
+            print('you lose your final score is ', score)
+    if ques == 'lower':
+        x = bool(c < n)
+        n = c
+        if x == True:
+            score = score + 1
+            print('correct your correct score is', score)
+        if x == False:
+            print('you lose your final score is ', score)
